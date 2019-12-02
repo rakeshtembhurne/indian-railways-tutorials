1822ef2242b774a4637df1d7bb3e45f1a7b42400
Rakesh Tembhurne <tj2point0@gmail.com>
2019-12-02 13:33:35 +0530

    Updates

---
+++ precache-manifest.2e6b4cbd1a28f758155d006a0579c072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91f5adc61793256b6f9b2cb129f9f8e",
+    "url": "/indian-railways-tutorials/index.html"
+  },
+  {
+    "revision": "f5ac5984580f3d064f85",
+    "url": "/indian-railways-tutorials/static/css/main.5c3bd433.chunk.css"
+  },
+  {
+    "revision": "6da839b9700ecf1d3c64",
+    "url": "/indian-railways-tutorials/static/js/2.e3a5cad2.chunk.js"
+  },
+  {
+    "revision": "f5ac5984580f3d064f85",
+    "url": "/indian-railways-tutorials/static/js/main.dc30a373.chunk.js"
+  },
+  {
+    "revision": "26cf978feaa9f9756346",
+    "url": "/indian-railways-tutorials/static/js/runtime-main.b5d6a9ef.js"
+  }
+]);
