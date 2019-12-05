66aa9805cb4df23320f928e7ca49a5059269fefc
Rakesh Tembhurne <tj2point0@gmail.com>
2019-12-05 17:31:37 +0530

    Added back previous listing

--- src/pages/TutorialsPage.js
+++ src/pages/TutorialsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Database } from '../store/Database';
-// import HorizontalMovieScroll from '../components/HorizontalMovieScroll';
+import HorizontalMovieScroll from '../components/HorizontalMovieScroll';
 
 
 export class TutorialsPage extends React.Component {
@@ -16,11 +16,7 @@ export class TutorialsPage extends React.Component {
             <div>
                 <br />
                 <div className="text-center">
-                    <div>
-                        {/* style={{padding:'56.25% 0 0 0', position:'relative'}} */}
-                        <iframe src='https://vimeo.com/showcase/6631234/embed' allowFullScreen frameBorder='0' style={{position:'absolute', top:0, left:0, width:'100%', height:'100%'}}></iframe>
-                    </div>
-                    {/* <HorizontalMovieScroll type="TRD Safety Tutorials" movies={this.state.movies} /> */}
+                    <HorizontalMovieScroll type="TRD Safety Tutorials" movies={this.state.movies} />
                 </div>
             </div>
         )
